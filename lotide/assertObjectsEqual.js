d34a709c63c884e04dd36da88fe056a2d9aef8ff
Your Name <you@example.com>
2021-07-22 22:19:29 +0000

    eqObj+ass

--- lotide/assertObjectsEqual.js
+++ lotide/assertObjectsEqual.js
@@ -1,11 +1,12 @@
 
 const assertEqual = function(actual, expected) {
-  if (actual != expected){
-   console.log(`Assertion Failed: ${actual} !== ${expected}`) 
+  const inspect = require('util').inspect;
+  if (!eqObj(actual, expected)){
+   console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`) 
   }
   else{
     
-    console.log(`Assertion Passed: ${actual} === ${expected}`) 
+    console.log(`Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`) 
   }
   };
   const assertArraysEqual = function(actual, expected) {
@@ -15,14 +16,37 @@ const assertEqual = function(actual, expected) {
   const eqObj = function(actual,expected){
     
    for (const i of Object.keys(actual)) {
-   
+   if ( actual[i].isArray || expected[i].isArray){
+    return  eqArrays (actual[i],expected[i])
+   } 
+   else
    {
-     if (actual[i] === expected [i]){}
+     if (actual[i] === expected[i]){}
      else
      {return false}
    }
    return true
   }
 }
+const eqArrays = function(actual,expected){
+  if (actual.length != expected.length){
+    return false;
+  }
+ for (let i = 0;i< actual.length ; i++){
+   if (actual[i] === expected [i]){}
+   else
+   {return false}
+ }
+ return true
+}
   //TEST
-  console.log(eqObj({ a: '1', b: 2 } , { b: 2, a: '1' }))
+  //assertEqual({ a: '1', b: 2 } , { b: 2, a: '1' })
+  const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual (cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual (cd, cd2); // => false
+  console. log(cd);
+  //const inspect = require('util').inspect;
+  //console.log(`Example label: ${inspect(cd2)}`);
