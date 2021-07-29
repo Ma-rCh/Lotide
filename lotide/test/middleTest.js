9620ab6d7578b022ab8677e580b956b0eb8ecc83
Your Name <you@example.com>
2021-07-29 03:24:28 +0000

    tests-4

--- lotide/test/middleTest.js
+++ lotide/test/middleTest.js
@@ -1,10 +1,19 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
-console.log(middle([1]))
-assertArraysEqual(middle([1]),[]) // => []
-assertArraysEqual(middle([1, 2]) ,[])// => []
-assertArraysEqual(middle([1, 2, 3]) ,[2])// => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]) ,[3])// => [3]
-assertArraysEqual(middle([1, 2, 3, 4]) ,[2, 3])// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]) ,[3, 4])// => [3, 4]
-//console.log(middle([1]))
+const assert = require('chai').assert;
+//const assert = require('chai').assert;
+describe("tail", () => {
+  //const result = tail(["Hello", "Lighthouse", "Labs"])
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  
+});
+// console.log(middle([1]))
+// assertArraysEqual(middle([1]),[]) // => []
+// assertArraysEqual(middle([1, 2]) ,[])// => []
+// assertArraysEqual(middle([1, 2, 3]) ,[2])// => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]) ,[3])// => [3]
+// assertArraysEqual(middle([1, 2, 3, 4]) ,[2, 3])// => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]) ,[3, 4])// => [3, 4]
+// //console.log(middle([1]))
--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,6 +1,14 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 const tail= require('../tail');
-  const result = tail(["Hello", "Lighthouse", "Labs"]);
-  assertEqual(result.length, 2); // ensure we get back two elements
-  assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-  assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const assert = require('chai').assert;
+describe("tail", () => {
+  //const result = tail(["Hello", "Lighthouse", "Labs"])
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs'] );
+  });
+  
+});
+  // const result = tail(["Hello", "Lighthouse", "Labs"]);
+  // assertEqual(result.length, 2); // ensure we get back two elements
+  // assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+  // assertEqual(result[1], "Labs"); // ensure second element is "Labs"
